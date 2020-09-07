c7b023055c8fa6d6947f3a6e16613d25514486e8
Martin Meluch <martin.meluch@gmail.com>
2020-09-07 18:14:24 +0200

    mime type validatot

--- src/app/posts/post-create/mime-type.validator.ts
+++ src/app/posts/post-create/mime-type.validator.ts
@@ -4,12 +4,11 @@ import { Observable, Observer, of } from "rxjs";
 export const mimeType = (
   control: AbstractControl
 ): Promise<{ [key: string]: any }> | Observable<{ [key: string]: any }> => {
-  if(typeof(control.value) === 'string') {
+  if(typeof(control.value) === "string") {
     return of(null);
   }
   const file = control.value as File;
   const fileReader = new FileReader();
-  // tslint:disable-next-line: deprecation
   const frObs = Observable.create(
     (observer: Observer<{ [key: string]: any }>) => {
       fileReader.addEventListener("loadend", () => {
